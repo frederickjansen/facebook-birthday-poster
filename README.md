1fcc7522c2816a0e5222c1d9d17196a0406c51d2
Frederick Jansen <frederick.jansen@gmail.com>
2013-05-28 12:07:44 +0200

    Replaced JS injection with an xhr request, which is easier and seems to work better Updated readme

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ facebook-birthday-poster
 ========================
 
 An automated poster for birthdays on Facebook, because nothing says "empty gesture" more than an automated, generic birthday wish on Facebook.
+
+Offers customizable messages, of which a random one will be picked.
--- facebook-birthday-poster.js
+++ facebook-birthday-poster.js
@@ -1,102 +1,53 @@
 var messages,
 birthdays,
-container;
+container,
+injected = false;
 
 // Five second delay because the birthday box is only generated after Facebook has fully loaded
+// Two seconds is usually enough, but apparently not always
 setTimeout(function() {
 	birthdays = getBirthdayForms();
-
 	// Check if any forms have been found
 	if (typeof birthdays !== "undefined" && birthdays.length > 0) {
-		container = getContainer();
-		triggerContainer();
 		// Loop over them
 		birthdays.forEach(function(form) {
 			// Chrome storage is asynchronous, so we have to use a callback instead of return
-			getRandomMessage($("textarea", form));
+			getRandomMessage($("textarea", form), form);
 		});
+		// Hide the birthdays after we're done
+		// TODO: Replace the textarea with a message saying you've responded to the birthday
+		$("#birthday_reminders_link").classList.add("hidden_elem");
 	}
 }, 5000);
 
-function getRandomMessage(textarea) {
+function getRandomMessage(textarea, form) {
 	// Only run if messages haven't been retrieved earlier
 	if (typeof messages === "undefined") {
 		// Get the array of messages from storage
 		chrome.storage.sync.get("messages", function(items) {
 			messages = items.messages;
 			// Submit the form with a random message
-			submitForm(textarea, messages[Math.floor(Math.random() * messages.length)]);
+			submitForm(textarea, form, messages[Math.floor(Math.random() * messages.length)]);
 		});
 	} else {
-		submitForm(textarea, messages[Math.floor(Math.random() * messages.length)]);
+		submitForm(textarea, form, messages[Math.floor(Math.random() * messages.length)]);
 	}
 }
 
-// We don't submit the form itself, since that breaks ajax
-// Instead we simulate a return on the textarea and let Facebook handle the rest
-function submitForm(textarea, message) {
+// We use an async xhr request to submit the form
+function submitForm(textarea, form, message) {
 	// Change the value of the textarea inside the form
 	textarea.value = message;
-	// Inject the code that submits the message into the DOM
-	injectJS();
-}
-
-// Because Chrome extensions run inside of a sandbox, triggering the return from inside the content script doesn't work
-// Instead we inject the code directly into the DOM and run it from there
-function injectJS() {
-	var body = document.getElementsByTagName('body')[0],
-    script = document.createElement('script');
-
-    script.textContent = '(' + toInject + ')()';
-    body.appendChild(script);
-}
-
-function toInject() {
-	var el = document.querySelector(".fbRemindersBirthdayListItem textarea");
-	// Trigger a return keyboard event on the textarea inside the form
-	triggerKeyboardEvent(el, 13);
-
-	// Calling submit() on a form breaks ajax, so we simulate a return keypress and let the website handle the rest
-	function triggerKeyboardEvent(element, keyCode) {
-		var eventObj = document.createEvent("KeyboardEvent");
-		eventObj.initEvent("keydown", true, true);
-		eventObj.keyCode = keyCode;
-		eventObj.which = keyCode;
-
-		element.dispatchEvent(eventObj); 
-	}
+	// The xhr request
+	var oReq = new XMLHttpRequest();
+	oReq.open("POST", form.action, true);
+	oReq.send(new FormData(form));
 }
 
 function getBirthdayForms() {
 	// Get all forms of birthdays
 	var elements = $$(".fbRemindersBirthdayListItem form", $(".fbBirthdayRemindersContent"));
 	return elements;
-}
-
-// We need the container to be able to apply the class hidden_elem to it
-function getContainer() {
-	// This turns u_ps_0_2_6 into u_ps_0_2_0, the id of the parent container
-	var el = birthdays[0].id;
-	el = el.slice(0, -1);
-	el += "0";
-	el = $("#"+el);
-	return el;
-}
-
-// The form is only fully instantianed after clicking on the birthday reminders link
-// We simulate this click and then instantly hide the container again
-function triggerContainer() {
-	var link = $("#birthday_reminders_link");
-	triggerMouseEvent(link);
-	container.classList.add("hidden_elem");
-}
-
-// Trigger a mouse click event
-function triggerMouseEvent(element) {
-	var eventObj = document.createEvent("MouseEvent");
-	eventObj.initEvent("click", true, true);
-
-	element.dispatchEvent(eventObj);
 }
 
 // Select first occurence
